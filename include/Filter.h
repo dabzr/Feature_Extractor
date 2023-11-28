028e87f1cc226cb86bd9daee7da023d383eee78f
dylanabzr <dylanab@pm.me>
2023-11-28 20:55:46 -0300

    Melhora de performance na alocação de memória na função 'createMatrix' e na liberação de memória na função 'freeMatrix'.

--- include/Filter.h
+++ include/Filter.h
@@ -1,8 +1,9 @@
+#pragma once
 #include "Image.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 struct Image filterAverage(struct Image*, int);
 unsigned char matrixAverage(unsigned char**, int);
-unsigned char **createMatrix(unsigned char**, int);
-void freeMatrix(unsigned char**, int);
+unsigned char **createMatrix(int);
+void freeMatrix(unsigned char**, unsigned char*);
--- src/Filter.c
+++ src/Filter.c
@@ -3,43 +3,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-unsigned char **createMatrix(unsigned char** matrix, int factor){
-  if(!(matrix = (unsigned char**) malloc(sizeof(unsigned char*) * factor))){
-    puts("Memória insuficiente!");
-    exit(1);
-  }
-
-  for(unsigned i = 0; i < factor; i++)
-    if (!(*(matrix + i) = (unsigned char*) malloc(sizeof(unsigned char) * factor))){ 
-      puts("Memória insuficiente!");
-      exit(i + 2);
-    }
-
-  return matrix;
-}
-
-unsigned char matrixAverage(unsigned char** matrix, int factor) {
-   int sum = 0;
-   for (int i = 0; i < factor; i++) {
-      for (int j = 0; j < factor; j++) {
-         sum += matrix[i][j];
-      }
-   }
-   return sum / (factor * factor);
-}
-
 struct Image filterAverage(struct Image* image, int factor) {
   if(!(factor % 2)){
     puts("É necessário que a matriz tenha tamanho ímpar.");
     exit(1);
   }
-   struct Image filtered_image = *image;
-   int sum_index, sum_data;
-   int coeficient = factor / 2;
-   unsigned char** matrix = NULL;
-   matrix = createMatrix(matrix, factor);
+  struct Image filtered_image = *image;
+  int sum_index, sum_data;
+  int coeficient = factor / 2;
+  unsigned char** matrix = NULL;
+  matrix = createMatrix(factor);
 
-   for (unsigned i = 0; i < image->size; i++) {
+  for (unsigned i = 0; i < image->size; i++) {
       int posX = -1 * coeficient;
       for (int x = 0; x < factor; x++) {
          int posY = (-image->w) * coeficient;
@@ -55,12 +30,36 @@ struct Image filterAverage(struct Image* image, int factor) {
       filtered_image.data[i] = matrixAverage(matrix, factor);
    }
 
-   freeMatrix(matrix, factor);
+   freeMatrix(matrix, matrix[0]);
    return filtered_image;
 }
 
-void freeMatrix(unsigned char** matrix, int factor){
-  for(unsigned i = 0; i < factor; i++)
-    free(*(matrix + i));
-  free(matrix);
+
+unsigned char matrixAverage(unsigned char** matrix, int factor) {
+   int sum = 0;
+   for (int i = 0; i < factor; i++) {
+      for (int j = 0; j < factor; j++) {
+         sum += matrix[i][j];
+      }
+   }
+   return sum / (factor * factor);
+}
+
+unsigned char **createMatrix(int factor) {
+    unsigned char **matrix = (unsigned char **)malloc(sizeof(unsigned char *) * factor); 
+    unsigned char *data = (unsigned char *)malloc(sizeof(unsigned char) * factor * factor);
+    if (!matrix || !data) {
+        puts("Memória insuficiente!");
+        exit(1);
+    }
+    for (int i = 0; i < factor; i++) {
+        matrix[i] = &data[i * factor];
+    }
+
+    return matrix;
+}
+
+void freeMatrix(unsigned char **matrix, unsigned char *data) {
+    free(data);
+    free(matrix);    
 }
