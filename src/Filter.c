fad7e63929c92888a37c2988dce419503da15807
dylanabzr <dylanab@pm.me>
2023-12-04 18:54:12 -0300

    Correção do ultimo commit

--- src/Filter.c
+++ src/Filter.c
@@ -10,24 +10,25 @@ struct pgm filterAverage(struct pgm image, int factor) {
   }
   int squared_factor = factor * factor;
   struct pgm filtered_image = image;
-  int sum_index, sum_data;
+  int sum_index, sum_data, border_verifier_1, border_verifier_2;
   int coeficient = factor / 2;
 
 
-  for (unsigned i = 0; i < image.size; i++) {
+  for (unsigned i = image.w * coeficient; i < (image.size - (image.w * coeficient)); i++) {
+    if((((i + coeficient) % image.w) - (coeficient + coeficient)) >= 0 ){
       sum_data = 0;
       int posX = -coeficient;
       for (int x = 0; x < factor; x++) {
          int posY = (-image.w) * coeficient;
          for (int y = 0; y < factor; y++) {
-            sum_index = i + posY + posX; 
-            if (sum_index >= 0 && sum_index <= image.size) 
-              sum_data += image.data[sum_index];
+            sum_index = i + posY + posX;
+                sum_data += image.data[sum_index];
             posY += image.w;
          }
          posX++;
       }
       filtered_image.data[i] = sum_data/squared_factor;
+    }
    }
 
    return filtered_image;
