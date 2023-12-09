a6913764e2a0a0c1eb1f1b5a2cc0fb02b5424b71
dylanabzr <dylanab@pm.me>
2023-12-09 09:11:38 -0300

    Adicionando mais comentários

--- src/Filter.c
+++ src/Filter.c
@@ -11,26 +11,26 @@ struct pgm filterAverage(struct pgm image, int factor) {
   int squared_factor = factor * factor;
   struct pgm filtered_image = image;
   int sum_index, sum_data;
-  int coeficient = factor / 2;
+  int coeficient = factor / 2; // Coeficiente que representa quantos pixels cada pixel tem de um lado
 
 
   for (unsigned i = image.w * coeficient; i < (image.size - (image.w * coeficient)); i++) { // Ignora pixels que fugiriam dos limites da imagem em cima e embaixo
     if((((i + coeficient) % image.w) - (coeficient + coeficient)) >= 0 ){ // Ignora pixels que fugiriam dos limites da imagem na esquerda e na direita
       sum_data = 0;
-      int posX = -coeficient;
+      int posX = -coeficient; // O "X" da matriz varia de -coeficiente até coeficiente
       for (int x = 0; x < factor; x++) {
-         int posY = (-image.w) * coeficient;
+         int posY = (-image.w) * coeficient; // O "Y" também varia de -coeficiente até coeficiente porém para pular/voltar linha você utiliza o tamanho da linha (image.w)
          for (int y = 0; y < factor; y++) {
-            sum_index = i + posY + posX;
-            sum_data += image.data[sum_index];
+            sum_index = i + posY + posX; // Atualiza o índice de acordo com a posição na matriz de filtro 
+            sum_data += image.data[sum_index]; //Soma cada item da matriz de filtro
             posY += image.w;
          }
          posX++;
       }
-      filtered_image.data[i] = sum_data/squared_factor;
+      filtered_image.data[i] = sum_data/squared_factor; // Faz a média e substitui no pixel da imagem filtrada
     }
   }
 
-   return filtered_image;
+   return filtered_image; // retorna a imagem filtrada
 }
 
--- src/SCM.c
+++ src/SCM.c
@@ -1,19 +1,21 @@
 #include "../include/SCM.h"
 
 unsigned char **CreateSCM(struct pgm img, struct pgm filtered_img, unsigned char level){
-  unsigned char **matrix = (unsigned char **) malloc(sizeof(unsigned char *) * level);
-  unsigned char *data = (unsigned char *)calloc(sizeof(unsigned char), level * level);
+  unsigned char **matrix = (unsigned char **) malloc(sizeof(unsigned char *) * level); // Aloca a matriz
+  unsigned char *matrix_vector = (unsigned char *)calloc(sizeof(unsigned char), level * level); 
+  /* Aloca um vetor no qual estarão os itens da matriz, todos zerados (calloc()) visto que cada item da matriz vai ser "incrementado" a cada mesma ocorrência dele*/
   for (int i = 0; i < level; i++) {
-    matrix[i] = &data[i * level];
+    matrix[i] = &matrix_vector[i * level]; // Assinala o inicio de cada linha da matriz como sendo o matrix_vector[i * level] (level sendo o fator da SCM)
   }
 
   for (unsigned long i = 0; i < img.size; i++){
-    matrix[(img.data[i])%level][(filtered_img.data[i])%level]++;
+    matrix[(img.data[i])%level][(filtered_img.data[i])%level]++; 
+    /*Quantiza os valores dos pixels da imagem original e da imagem filtrada e incrementa os "encontros"*/ 
   }
   return matrix;
 }
 
 void freeMatrix(unsigned char **matrix) {
-    free(matrix[0]);
-    free(matrix);
+    free(matrix[0]); // Libera a memoria alocada no vetor 
+    free(matrix); // Libera a memória alocada na matriz
 }
